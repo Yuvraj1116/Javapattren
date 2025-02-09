1c628e9747ba5bb221d99967b13f064814a965fb
Yuvraj Tiwari <tiwariyuvraj9109@gmail.com>
2025-02-09 21:12:21 +0530

    solid rohmbas

--- basic.java
+++ basic.java
@@ -72,28 +72,125 @@ package Javapattren;
 // }
 // }
 
-import java.util.*;
-public class basic{
-  public static int factorial(int n){
-    int fact=1;
-        for(int i=1; i<=n;i++){
-          fact = fact*i;
+// code is prime or not or also range in isPrime 
+
+// import java.util.*;
+// public class basic{
+
+//   public static void primeRange( int n){
+
+//     for(int i=2; i<=n; i++){
+//       if(isPrime(i)){
+//         System.out.println(i+"");
+//       };
+//     }
+//     System.out.println();
+//   }
+
+//   public static boolean isPrime(int n){
+//       boolean isPrime = true;
+//     for(int i=2; i<=n-1;i++){
+//         if(n%i==0){
+//           isPrime = false;
+//         }  
+//     }
+//       return isPrime;
+//     } 
+
+    
+//   public static void main(String args[]){
+//     Scanner sc = new Scanner(System.in);
+
+//     System.out.print("Enter number to check isPrime or not :");
+//     int n = sc.nextInt();
+//     primeRange(n);
+// }
+// }
+
+
+// Binary to Decimal Code 
+//   public  class basic{
+//   public static void BinaryToDecimal(int binNum){
+//     int pow = 0; 
+//     int decNum = 0;
+//     int decimalNum = binNum;
+
+//     while(binNum > 0){
+//       int lastDigit  = binNum %10;
+//         decNum = decNum+(lastDigit *(int)Math.pow(2, pow));
+//         pow++;
+//         binNum = binNum/10;
+
+//     }
+//     System.out.println(decNum);
+//   }
+//   public static void main(String args[]){
+//       BinaryToDecimal(101);
+//     }
+// }
+
+
+//DEcimal to Binary converstion
+
+// public  class basic {
+//   public static void DecimalToBinary(int n){
+//        int pow = 0;
+//        int binary = 0; 
+//        while(n>0){
+//         int rem =  n %2;
+//         binary =  binary + rem* (int)Math.pow(10, pow);
+//         pow ++;
+//          n = n/2;
+//        }
+//        System.out.println(binary);
+//       }
+
+//       public static void main (String args[]){
+//           DecimalToBinary(5);
+//       }
+// }
+
+
+//HOLLOW RECTANGLE STAR PATTERN CODE 
+// public class basic{
+
+//   public static void hollow_Rectangle(int totRows, int totCols){
+//   for(int i=1; i <=totRows; i++){
+//       for(int j=1; j<=totCols; j++){
+//         if(i ==1 || i== totRows || j == 1|| j == totCols){
+//           System.out.print("*");
+//         }else{
+//           System.out.print(" ");
+//         }
+//       }
+//       System.out.println();
+//  }
+//   }
+//   public static void main (String args[]){
+//     hollow_Rectangle(10, 10);
+//   }
+
+// }
+
+
+//INVERTED & ROTATED HALF-PYRAMID WITH NUMBERS
+ 
+public class basic {
+
+  public static void Butterfly(int n){
+      for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+          System.out.print(" ");
+        }
+        for(int j=1; j<=n; j++){ 
+          System.out.print("*"+ " ");
+        }
+        System.out.println();
       }
-      return fact;
-    } 
+  }
 
-    public static int binCoff(int n, int r ){
-      int fac_n =factorial(n);
-      int fac_r = factorial(r);
-      int nmr_fac = factorial(n-r);
+public static void main(String args[]){
+  Butterfly(10);
+} 
 
-        int binoCoff = fac_n/(fac_r*nmr_fac);
-        return binoCoff;
-    }
-  public static void main(String args[]){
-    System.out.println(binCoff(5, 2));
-            
-   int faco= factorial(18);
-   System.out.println("Factorial of 18 is :"+faco );
-}
 }

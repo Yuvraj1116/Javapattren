fcad906ee55f8020c6f1a206a102b12aadbba789
Yuvraj Tiwari <tiwariyuvraj9109@gmail.com>
2025-02-24 23:44:25 +0530

    solve buy sell stock problem

--- array.java
+++ array.java
@@ -252,41 +252,88 @@
 // }
 
 
-// Trzpping Rain WAter Question 
+// Trapping Rain WAter Question 
 
-
-
-public class array {
-    public static void trappingRainwater(int height[]){
+// public class array {
+//     public static void trappingRainwater(int height[]){
       
-        // CAlculate left max boundary -helper array
-      int leftMax[] = new int [height.length];
-         leftMax [0] = height[0];
-         for( int i=1; i<height.length;i++){
-            leftMax[i] = Math.max(height[i], leftMax[i-1]);
-         }
+//         // CAlculate left max boundary -helper array
+//       int leftMax[] = new int [height.length];
+//          leftMax [0] = height[0];
+//          for( int i=1; i<height.length;i++){
+//             leftMax[i] = Math.max(height[i], leftMax[i-1]);
+//          }
 
-         //calculate Right Max boundary -helper array
-         int rightMax[] = new int [height.length];
-         rightMax[height.length-1] = height[height.length-1];
-          for(int i= height.length-2; i>=0; i-- ){
-            rightMax[i] = Math.max(height[i],rightMax[i+1]);
-          }
+//          //calculate Right Max boundary -helper array
+//          int rightMax[] = new int [height.length];
+//          rightMax[height.length-1] = height[height.length-1];
+//           for(int i= height.length-2; i>=0; i-- ){
+//             rightMax[i] = Math.max(height[i],rightMax[i+1]);
+//           }
        
-          // Loop For measuring WaterLevel
-          int trappedWater = 0; 
-          for(int i=0;i<height.length;i++){
-            // WaterLevel = min(LeftMax , rightMAx)
-            int waterLevel = Math.min(leftMax[i],rightMax[i]);
-            //trapped water = waterLevel - heigth[i]
-            trappedWater += waterLevel - height[i]; 
-          }
-          System.out.println(trappedWater); 
+//           // Loop For measuring WaterLevel
+//           int trappedWater = 0; 
+//           for(int i=0;i<height.length;i++){
+//             // WaterLevel = min(LeftMax , rightMAx)
+//             int waterLevel = Math.min(leftMax[i],rightMax[i]);
+//             //trapped water = waterLevel - heigth[i]
+//             trappedWater += waterLevel - height[i]; 
+//           }
+//           System.out.println(trappedWater); 
 
-    }
+//     }
 
-     public static void main(String args[]){
-        int height[] = {4,2,0,6,3,2,5};
-        trappingRainwater(height);
-     }
-}
+//      public static void main(String args[]){
+//         int height[] = {4,2,0,6,3,2,5};
+//         trappingRainwater(height);
+//      }
+// }
+
+// Buy And Sell Stock Code
+
+// public class array {
+//     public static int BuySellStock(int price[]){
+//       int buyPrice = Integer.MAX_VALUE;
+//       int maxProfit = 0;
+
+//       for(int i=0; i<price.length;i++){
+//           if(buyPrice <price[i]){
+//             int profit = price[i] - buyPrice;
+//              maxProfit = Math.max(maxProfit, profit);
+
+//           }else{
+//             buyPrice = price[i];
+//           }
+//       }
+//         return maxProfit;
+
+//     }
+ 
+
+
+//   public static void main(String args[]){
+//      int price[] = {7,1,5,3,6,4};
+//       int profit =  BuySellStock(price);
+//       System.out.println(profit);
+//   }
+// }
+
+// Product Puzzle  time complexcity   (brute froce)
+// public class array{
+//   public static void main(String args[]){
+//     int arr [] = {1,2,3,4,5}; 
+    
+//     int res [] = new int[arr.length];    
+//     for(int i=0;i<arr.length;i++){
+//           int product = 1;
+//       for(int j=0; j<arr.length;j++){
+//           if(j!=i){
+//         product = product *arr[j];
+//           }
+//       }
+//       res[i] = product;
+//       System.out.print (res[i] +" ");
+//     }
+//   } 
+// }
+

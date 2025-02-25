25a9149a72b7d3a09c1ff6169e03de54fc1f4134
Yuvraj Tiwari <tiwariyuvraj9109@gmail.com>
2025-02-25 16:09:56 +0530

    learning prefix suffix array with solving productPuzzle question

--- array.java
+++ array.java
@@ -337,3 +337,46 @@
 //   } 
 // }
 
+// Product Puzzle  time complexcity (using prefix suffix array) optimized code
+
+public class array {
+    public  int[] productPuzzle(int [] nums){
+        int n = nums.length;
+        int [] prefix = new int [n];
+        int [] suffix = new int [n];
+
+        prefix[0] = nums[0];
+        for(int i =1; i<n; i++){
+            prefix[i] = prefix[i-1] * nums[i];
+        }
+
+        suffix[n-1] = nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            suffix[i] = suffix[i+1] *nums[i];
+        }
+        int []res = new int[n];
+        for(int i=1; i<n-1;i++){
+            res[i] = prefix[i-1] * suffix [i+1];
+        }
+
+        res[0] = suffix[1];
+        res[n-1] = prefix[n-2];
+        
+        return res;
+    } 
+
+
+    public static void main (String args[]){
+        
+         array s1 =  new array();
+        
+        int [] nums = {1,2,3,4,5};
+
+        int [] res = s1.productPuzzle(nums);
+
+        for(int i=0 ; i<res.length;i++){
+            System.out.println(res[i]);
+        }
+
+    }
+}
